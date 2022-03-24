15e8e5e180672019a576a8606281de51334f813d
Ramya-QC <bablu.ramya123@gmail.com>
2022-03-24 20:09:49 +0530

    first commit to jenkins project

---
+++ src/main/java/SeleniumProject/jenkinsproject/App.java
@@ -0,0 +1,13 @@
+package SeleniumProject.jenkinsproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/SeleniumProject/jenkinsproject/TestClass.java
@@ -0,0 +1,10 @@
+package SeleniumProject.jenkinsproject;
+
+import org.testng.annotations.Test;
+
+public class TestClass {
+@Test
+public void m()
+{
+	System.out.println("hel");}
+}
